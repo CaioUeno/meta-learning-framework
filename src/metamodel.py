4a1318f9a558c64f79ffe8b3c4c2cc9928ce5614
Caio Ueno <caioluiggy@hotmail.com>
2020-09-25 15:02:43 -0300

    working for clf

--- src/metamodel.py
+++ src/metamodel.py
@@ -1,3 +1,7 @@
+from sklearn.model_selection import cross_val_predict
+from statistics import mode
+import numpy as np
+
 class MetaLearningModel(object):
 
     def __init__(self, metamodel, base_models, task, mode):
@@ -10,7 +14,7 @@ class MetaLearningModel(object):
 
     def check_args(self, metamodel, base_models, task, mode):
 
-        if task != 'classification' or task != 'regression':
+        if task not in ['classification', 'regression']:
             raise ValueError('Must choose a task: classification or regression.')
 
         if task == 'regression' and mode == 'binary':
@@ -21,27 +25,86 @@ class MetaLearningModel(object):
             raise TypeError('metamodel must have method fit(X, y) and predict(X).')
 
         for base_model in base_models:
-            if 'fit' not in dir(base_models) or 'predict' not in dir(base_models):
+            if 'fit' not in dir(base_model) or 'predict' not in dir(base_model):
                 raise TypeError('base models must have method fit(X, y) and predict(X).')
 
         return True
 
-    def fit(self, X_y_base_models, X_y_meta_model):
+    def fit_both(self, X_y_base_models, X_y_meta_model):
 
         X_base_models, y_base_models = X_y_base_models
-        for base_model in base_models:
-            base_model.fit(X_base_models, y_base_models)
+        self.__fit_base_models(X_base_models, y_base_models)
 
         X_metamodel, y_metamodel = X_y_meta_model
-        y_metamodel = self.prepare_meta_target(y_metamodel)
+        self.__fit_metamodels(X_metamodel, y_metamodel)
+
+    def __fit_base_models(self, X, y):
+
+        for base_model in self.base_models:
+            base_model.fit(X, y)
+
+    def __predict_base_models(self, X, selected_base_models):
+
+        predictions = []
+        for idx, base_model in enumerate(self.base_models):
+            if selected_base_models[idx] == 1:
+                predictions.append(base_model.predict(X).ravel()[0])
+
+        return predictions
+
+    def __fit_metamodels(self, X, y):
+
         for idx, metamodel in enumerate(self.metamodels):
-            metamodel.fit(X_metamodel, y_metamodel[idx])
+            metamodel.fit(X, y[:, idx])
 
-    def __fit_base_models(self, X_y_base_models):
+    def __predict_metamodels(self, X):
 
-        X_base_models, y_base_models = X_y_base_models
-        for base_model in base_models:
-            base_model.fit(X_base_models, y_base_models)
+        predictions = np.zeros((len(X), len(self.metamodels)))
+        for idx, metamodel in enumerate(self.metamodels):
+            predictions[:, idx] = metamodel.predict(X)
 
+        return predictions.ravel().tolist()
 
-    # def predict(self, X):
+    def fit(self, X, y, n_folds):
+
+        X_metamodels, y_metamodels = self.__cross_validation(X, y, n_folds=10)
+        self.fit_both((X, y), (X_metamodels, y_metamodels))
+
+    def predict(self, X):
+
+        predictions = []
+        for x in X:
+            selected_base_models = self.__predict_metamodels([x])
+            if not np.any(selected_base_models):
+                selected_base_models[0] = 1
+            # print(self.__predict_base_models([x], selected_base_models))
+            final_prediction = self.__combiner(self.__predict_base_models([x], selected_base_models))
+            predictions.append(final_prediction)
+
+        return predictions
+
+    def __cross_validation(self, X, y, n_folds):
+
+        base_models_predictions = {}
+        for idx, base_model in enumerate(self.base_models):
+            base_models_predictions[idx] = cross_val_predict(base_model, X, y, cv=n_folds, method='predict_proba')
+
+        if self.mode == 'binary':
+
+            y_target_metamodels = np.zeros((y.shape[0], len(self.base_models)))
+            for idx, base_model in enumerate(self.base_models):
+                y_target_metamodels[:, idx] = (np.argmax(base_models_predictions[idx], axis=1) == y).astype(int)
+
+            return X, y_target_metamodels
+
+    def __adapt_method(self):
+
+        if self.task == 'regression' or (self.task == 'classification' and self.mode == 'binary'):
+            return 'predict'
+
+        else:
+            return 'predict_proba'
+
+    def __combiner(self, labels):
+
+        return mode(labels)
