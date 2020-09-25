11ce54450d98aac8f32cfe1f8095cbf01b0e128f
Caio Ueno <caioluiggy@hotmail.com>
2020-09-25 15:03:18 -0300

    code with usage example

---
+++ src/example.py
@@ -0,0 +1,13 @@
+from metamodel import MetaLearningModel
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+X = load_iris().data
+y = load_iris().target
+
+model = MetaLearningModel(DecisionTreeClassifier(), [KNeighborsClassifier(n_neighbors=3), KNeighborsClassifier(n_neighbors=7), KNeighborsClassifier(n_neighbors=11)],
+                          'classification', 'binary')
+
+model.fit(X, y, 10)
+print(model.predict(X))
