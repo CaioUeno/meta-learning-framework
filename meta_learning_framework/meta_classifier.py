e52ef4855cab9f98c347607c13da36e3f8c70eb2
Caio Ueno <caioluiggy@hotmail.com>
2021-02-12 16:56:04 -0300

    adding doc string about y shape on fit method

--- meta_learning_framework/meta_classifier.py
+++ meta_learning_framework/meta_classifier.py
@@ -17,6 +17,14 @@ class MetaClassifier(BaseEstimator):
 
     @abstractmethod
     def fit(self, X, y):
+
+        """
+        Note: y will have shape (n_instances, n_base_models), even if it is only a multi-class task (not a multi-label).
+        Keep this in mind while implementing this method. 
+        Suggestion: if you want a "one-number label" (0, 1, 2, ...) and not a one-hot encoding,
+        simply apply numpy.argmax(y) and then you have a numerical class.
+        """
+
         pass
 
     @abstractmethod
