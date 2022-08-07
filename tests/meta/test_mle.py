d8071e75ecf6f380c8df71efbc9c8662ddc2b7e2
Caio Ueno <caioluiggy@hotmail.com>
2022-08-07 14:12:57 -0300

    delete some files

--- tests/meta/test_mle.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from meta_learning_framework.exceptions import InheritanceError
-from meta_learning_framework.meta import MetaLearningEnsemble
-
-
-class TestMetaLearningEnsembleClassification:
-    def test_fit_method(self, classification_sample):
-
-        pass
